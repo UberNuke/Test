4464fe07c3540f0c79dc142e12ca33f2894c9740
Razvan G <razvan.gir@gmail.com>
2014-10-26 17:51:03 +0200

    another commit

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.test;
 
-//import android.support.v7.app.ActionBarActivity;
-//import android.os.Bundle;
-//import android.view.Menu;
-//import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
